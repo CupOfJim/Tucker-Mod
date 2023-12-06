2ed4bdbfc0b8b16597cad064cdd551df06c53a04
clay <claytonharting+github@gmail.com>
2023-12-06 02:13:32 -0500

    implemented buffer

---
+++ BufferController.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuckerTheSaboteur.actions;
+
+namespace TuckerTheSaboteur
+{
+    [HarmonyPatch(typeof(Ship))]
+    public static class BufferController
+    {
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Combat.SendCardToExhaust))]
+        public static void HarmonyPostfix_Ship_OnAfterTurn(Ship __instance, State s, Combat c)
+        {
+            if (__instance.Get((Status)MainManifest.statuses["buffer"].Id) > 0)
+            {
+                c.QueueImmediate(new AApplyBuffer
+                {
+                    targetPlayer = __instance.isPlayerShip
+                });
+            }
+        }
+    }
+}
---
+++ actions/AApplyBuffer.cs
@@ -0,0 +1,35 @@
+﻿using FSPRO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuckerTheSaboteur;
+
+namespace TuckerTheSaboteur.actions
+{
+    // Copied from ACorrodeDamage
+    // Do not put this on a card, unless you want the card to trigger fuel leak instantly
+    public class AApplyBuffer : CardAction
+    {
+        public bool targetPlayer;
+
+        public override void Begin(G g, State s, Combat c)
+        {
+            timer *= 2.0;
+            Ship ship = (targetPlayer ? s.ship : c.otherShip);
+            int bufferAmount = ship.Get((Status)MainManifest.statuses["buffer"].Id);
+            if (ship != null)
+            {
+                c.QueueImmediate(new AStatus()
+                {
+                    status = Enum.Parse<Status>("tempShield"),
+                    targetPlayer = targetPlayer,
+                    statusAmount = bufferAmount
+                });
+                ship.PulseStatus((Status)MainManifest.statuses["buffer"].Id);
+                ship.Set((Status)MainManifest.statuses["buffer"].Id, bufferAmount - 1);
+            }
+        }
+    }
+}
