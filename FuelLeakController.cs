45861a74a94bebd84f1dfb98d6445150ef95d0aa
clay <claytonharting+github@gmail.com>
2023-12-06 02:00:27 -0500

    implemented fuel leak

---
+++ FuelLeakController.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuckerTheSaboteur
+{
+    [HarmonyPatch(typeof(AMove))]
+    public static class FuelLeakController
+    {
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Combat.SendCardToExhaust))]
+        public static void HarmonyPostfix_AMove_Begin(AMove __instance, G g, State s, Combat c)
+        {
+            Ship ship = (__instance.targetPlayer ? s.ship : c.otherShip);
+            if (ship.Get((Status)MainManifest.statuses["fuel_leak"].Id) > 0)
+            {
+                c.QueueImmediate(new ACorrodeDamage
+                {
+                    targetPlayer = __instance.targetPlayer
+                });
+            }
+        }
+    }
+}
---
+++ actions/AFuelLeakDamage.cs
@@ -0,0 +1,29 @@
+﻿using FSPRO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuckerTheSaboteur;
+
+namespace TuckerTheSaboteur.actions
+{
+    // Copied from ACorrodeDamage
+    // Do not put this on a card, unless you want the card to trigger fuel leak instantly
+    public class AFuelLeakDamage : CardAction
+    {
+        public bool targetPlayer;
+
+        public override void Begin(G g, State s, Combat c)
+        {
+            timer *= 2.0;
+            Ship ship = (targetPlayer ? s.ship : c.otherShip);
+            if (ship != null)
+            {
+                ship.DirectHullDamage(s, c, ship.Get((Status)MainManifest.statuses["fuel_leak"].Id));
+                Audio.Play(Event.Status_CorrodeHurt);
+                ship.PulseStatus((Status)MainManifest.statuses["fuel_leak"].Id);
+            }
+        }
+    }
+}
