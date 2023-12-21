547920f33deb7cfd0e1cac16d79a795eaab3df9c
clay <claytonharting+github@gmail.com>
2023-12-21 13:43:19 -0500

    started work on adding render transpiler

--- OffsetAttackPatches.cs
+++ OffsetAttackPatches.cs
@@ -1,8 +1,12 @@
 ﻿using HarmonyLib;
+using Nanoray.Shrike.Harmony;
+using Nanoray.Shrike;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -42,5 +46,82 @@ namespace TuckerTheSaboteur
 
             return false;
         }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Card), nameof(Card.RenderAction))]
+        private static IEnumerable<CodeInstruction> HarmonyTranspile_Card_RenderAction(IEnumerable<CodeInstruction> instructions, ILGenerator il, MethodBase originalMethod)
+        {
+            try
+            {
+                var localVars = originalMethod.GetMethodBody()!.LocalVariables;
+
+                return new SequenceBlockMatcher<CodeInstruction>(instructions)
+                    .Find(
+                        ILMatches.Ldloc<List<Card>>(localVars),
+                        ILMatches.AnyLdloc,
+                        ILMatches.Call("AddRange")
+                    )
+                    .PointerMatcher(SequenceMatcherRelativeElement.First)
+                    .CreateLdlocInstruction(out var cardList)
+                    .Advance(2)
+                    .Insert(SequenceMatcherPastBoundsDirection.After, SequenceMatcherInsertionResultingBounds.JustInsertion,
+                        new List<CodeInstruction>
+                        {
+                        new(OpCodes.Ldarg_0),
+                        cardList,
+                        new(OpCodes.Call, AccessTools.DeclaredMethod(typeof(CardBrowsePatch), nameof(InjectCards)))
+                        })
+                    .AllElements();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Card.RenderAction patch failed!");
+                Console.WriteLine(e);
+                return instructions;
+            }
+        }
+
+        //[HarmonyPrefix]
+        //[HarmonyPatch(typeof(AJupiterShoot), nameof(AJupiterShoot.Begin))]
+        //public static bool Begin(AJupiterShoot __instance, G g, State s, Combat c)
+        //{
+        //    __instance.timer = 0.0;
+        //    SortedList<int, CardAction> sortedList = new SortedList<int, CardAction>();
+        //    foreach (KeyValuePair<int, StuffBase> item in c.stuff)
+        //    {
+        //        if (!(item.Value is JupiterDrone))
+        //        {
+        //            continue;
+        //        }
+
+        //        int xOffset = 0;
+        //        if (__instance.attackCopy.fromX != null)
+        //        {
+        //            int baseX = s.ship.parts.FindIndex((Part p) => p.type == PType.cannon && p.active);
+        //            xOffset = (int)(__instance.attackCopy.fromX - baseX);
+        //        }
+
+        //        AAttack aAttack = Mutil.DeepCopy(__instance.attackCopy);
+        //        aAttack.fast = true;
+        //        aAttack.fromX = null;
+        //        aAttack.fromDroneX = item.Value.x + xOffset;
+        //        aAttack.targetPlayer = item.Value.targetPlayer;
+        //        aAttack.shardcost = 0;
+        //        int damage = aAttack.damage;
+        //        foreach (Artifact item2 in s.EnumerateAllArtifacts())
+        //        {
+        //            aAttack.damage += item2.ModifyBaseJupiterDroneDamage(s, c, item.Value);
+        //            if (aAttack.damage > damage)
+        //            {
+        //                aAttack.artifactPulse = item2.Key();
+        //                damage = aAttack.damage;
+        //            }
+        //        }
+        //        sortedList.Add(item.Value.x + xOffset, aAttack);
+        //    }
+        //    c.QueueImmediate(sortedList.Values);
+
+        //    return false;
+        //}
     }
 }
