81ff335177ae7b79d7c8d5a852918fca3cc1dc16
clay <claytonharting+github@gmail.com>
2023-12-22 06:25:59 -0500

    added icons to SprayandPray

--- cards/SprayandPray.cs
+++ cards/SprayandPray.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TuckerTheSaboteur.actions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TuckerTheSaboteur.cards
 {
@@ -12,87 +14,87 @@ namespace TuckerTheSaboteur.cards
         public override List<CardAction> GetActions(State s, Combat c)
         {
             Upgrade upgrade = base.upgrade;
-            switch (upgrade)
+
+            // handle attack offset
+            int cannonX = s.ship.parts.FindIndex((Part p) => p.type == PType.cannon && p.active);
+
+            // return final result
+            List<CardAction> actions = new();
+
+            if (upgrade == Upgrade.A)
             {
-                case Upgrade.None:
-                    return new List<CardAction> ()
-                    {
-                        new AAttack ()
-                        {
-                            fromX = -1,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                        },
-                        new AAttack ()
-                        {
-                            damage = GetDmg(s, 2),
-                            fast = true,
-                        },
-                        new AAttack ()
-                        {
-                            fromX = 1,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                        }
-                    };
-                case Upgrade.A:
-                    return new List<CardAction> ()
-                    {
-                        new AAttack ()
-                        {
-                            fromX = -2,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                        },
-                        new AAttack () {
-                            fromX = -1,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                        },
-                        new AAttack ()
-                        {
-                            damage = GetDmg(s, 2),
-                            fast = true,
-                        },
-                        new AAttack ()
-                        {
-                            fromX = 1,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                        },
-                        new AAttack () {
-                            fromX = 2,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                        }
-                    };
-                case Upgrade.B:
-                    return new List<CardAction> ()
-                    {
-                        new AAttack ()
-                        {
-                            fromX = -1,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                            piercing = true,
-                        },
-                        new AAttack ()
-                        {
-                            damage = GetDmg(s, 2),
-                            fast = true,
-                            piercing = true,
-                        },
-                        new AAttack ()
-                        {
-                            fromX = 1,
-                            damage = GetDmg(s, 1),
-                            fast = true,
-                            piercing = true,
-                        }
-                    };
+                actions.Add(new TuckerTheSaboteur.actions.AAttackNoIcon()
+                {
+                    fromX = cannonX - 2,
+                    damage = GetDmg(s, 1),
+                    fast = true,
+                    piercing = true,
+                });
             }
 
-            throw new Exception(this.GetType().Name + " was upgraded to something that doesn't exist.");
+            actions.Add(new TuckerTheSaboteur.actions.AAttackNoIcon()
+            {
+                fromX = cannonX - 1,
+                damage = GetDmg(s, 1),
+                fast = true,
+                piercing = (upgrade == Upgrade.B),
+            });
+            actions.Add(new TuckerTheSaboteur.actions.AAttackNoIcon()
+            {
+                damage = GetDmg(s, 2),
+                fast = true,
+                piercing = (upgrade == Upgrade.B),
+            });
+            actions.Add(new TuckerTheSaboteur.actions.AAttackNoIcon()
+            {
+                fromX = cannonX + 1,
+                damage = GetDmg(s, 1),
+                fast = true,
+                piercing = (upgrade == Upgrade.B),
+            });
+
+            if (upgrade == Upgrade.A)
+            {
+                actions.Add(new TuckerTheSaboteur.actions.AAttackNoIcon()
+                {
+                    fromX = cannonX + 2,
+                    damage = GetDmg(s, 1),
+                    fast = true,
+                    piercing = true,
+                });
+            }
+
+            int numAttacks = actions.Count;
+
+            foreach (var action in actions)
+            {
+                var aattack = action as AAttack;
+                int offset = (int)(aattack.fromX - cannonX);
+
+                Spr offsetSprite = offset > 0
+                    ? (Spr)MainManifest.sprites["icons/Offset_Shot_Right"].Id
+                    : (Spr)MainManifest.sprites["icons/Offset_Shot_Left"].Id;
+
+
+                Icon attackIcon = ABluntAttack.DoWeHaveCannonsThough(s)
+                    ? new Icon(Enum.Parse<Spr>(aattack.piercing ? "icons_attackPiercing" : "icons_attack"), aattack.damage, Colors.redd)
+                    : new Icon(Enum.Parse<Spr>(aattack.piercing ? "icons_attackPiercingFail" : "icons_attackFail"), aattack.damage, Colors.attackFail);
+
+                actions.Add(new TuckerTheSaboteur.actions.ATooltipDummy()
+                {
+                    tooltips = new() { }, // eventually put the tooltip for offset attacks here
+                    icons = new()
+                    {
+                        new Icon(offsetSprite, offset, Colors.redd),
+                        attackIcon
+                    }
+                });
+            }
+
+            for(int i = 0; i < numAttacks; i++) 
+            {
+                actions.Add(new ADummyAction());
+            }
         }
         public override CardData GetData(State state)
         {
