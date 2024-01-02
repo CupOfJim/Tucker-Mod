b18f088f6585928d683e9b0fa261dee75e2bcaf8
clay <claytonharting+github@gmail.com>
2024-01-02 15:19:39 -0500

    renamed art of war's file

--- tuckerArtifacts/ArtofWar.cs
+++ tuckerArtifacts/ArtofWar.cs
@@ -8,7 +8,7 @@ namespace TuckertheSabotuer.Artifacts
     {
         public override void OnTurnStart(State s, Combat c)
         {
-            int playerShield =  s.ship.Get(Enum.Parse<Status>("shield")) + s.ship.Get(Enum.Parse<Status>("tempShield"));
+            int playerShield = s.ship.Get(Enum.Parse<Status>("shield")) + s.ship.Get(Enum.Parse<Status>("tempShield"));
             if (playerShield == 0)
             {
                 c.QueueImmediate(new AAddCard()
