93f0748ea78e9c9b56b2237a0550251c8b6bb82f
clay <claytonharting+github@gmail.com>
2024-01-11 05:35:29 -0500

    recoil animations now work with offset attacksgit diff!

--- OffsetAttackPatches.cs
+++ OffsetAttackPatches.cs
@@ -93,5 +93,92 @@ namespace TuckerTheSaboteur
 
         //    return false;
         //}
+
+
+
+        private static int cannonAnimXOffset = 0;
+        private static HashSet<int> firingCannons = new();
+
+        //// EffectSpawner.Cannon(g, targetPlayer, raycastResult, dmg, isBeam);
+        //// prefix patch it to do nothing
+
+        //// aattack begin prefix: if not is volley and xoffset, set cannonAnimXOffset = xoffset (otherwise set it to 0)
+        //// effectspawner.cannon prefix- raycastResult.x -= cannonAnimXOffset
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(AAttack), nameof(AAttack.Begin))]
+        public static void CannonRecoilAnimationSetup(AAttack __instance, G g, State s, Combat c)
+        {
+            if (__instance.targetPlayer) return;
+            
+            if (!__instance.multiCannonVolley)
+            {
+                cannonAnimXOffset = 0;
+                if (!__instance.fromX.HasValue) return;
+
+                int cannonX = s.ship.parts.FindIndex((Part p) => p.type == PType.cannon && p.active);
+                cannonAnimXOffset = (__instance.fromX ?? 0) - cannonX;
+
+                bool willBeVolley = (!__instance.targetPlayer && !__instance.fromDroneX.HasValue && g.state.ship.GetPartTypeCount(PType.cannon) > 1 && !__instance.multiCannonVolley);
+                if (willBeVolley)
+                {
+                    firingCannons.Clear();
+                    return;
+                }
+
+                int? num = AAttack_GetFromX(__instance, s, c);
+                if (!num.HasValue) return;
+
+                Part partAtLocalX = s.ship.GetPartAtLocalX(num.Value);
+                if (partAtLocalX != null)
+                {
+                    partAtLocalX.pulse = 0.0;
+                }
+
+                int realPartX = num.Value - cannonAnimXOffset;
+                Part partAtRealLocalX = s.ship.GetPartAtLocalX(realPartX);
+                if (partAtRealLocalX != null)
+                {
+                    partAtRealLocalX.pulse = 1.0;
+                }
+
+            }
+            else
+            {
+
+                int? num = AAttack_GetFromX(__instance, s, c);
+                if (!firingCannons.Contains(num.Value))
+                {
+                    Part partAtLocalX = s.ship.GetPartAtLocalX(num.Value);
+                    if (partAtLocalX != null)
+                    {
+                        partAtLocalX.pulse = 0.0;
+                    }
+                }
+
+                int realPartX = num.Value - cannonAnimXOffset;
+                Part partAtRealLocalX = s.ship.GetPartAtLocalX(realPartX);
+                if (partAtRealLocalX != null)
+                {
+                    partAtRealLocalX.pulse = 1.0;
+                }
+                firingCannons.Add(realPartX);
+
+            }
+        }
+
+        private static int? AAttack_GetFromX(AAttack a, State s, Combat c)
+        {
+            if (a.fromX.HasValue)
+            {
+                return a.fromX;
+            }
+            int num = (a.targetPlayer ? c.otherShip : s.ship).parts.FindIndex((Part p) => p.type == PType.cannon && p.active);
+            if (num != -1)
+            {
+                return num;
+            }
+            return null;
+        }
+
     }
 }
